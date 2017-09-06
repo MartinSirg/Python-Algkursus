8fb22d20e751f593e8d8a4e9b1f8faced95804a9
masirg <masirg@ttu.ee>
2017-09-06 12:35:39 +0000

    Update hello.py

--- EX01A/hello.py
+++ EX01A/hello.py
@@ -3,11 +3,11 @@
 
 def hello():
     """
-        Write a function which prints message "Hello world!" to the console.
+    Write a function which prints message "Hello world!" to the console.
 
-        The function should not return anything or do anything else except printing the specified message.
-        :return: None
-        """
+    The function should not return anything or do anything else except printing the specified message.
+    :return: None
+    """
     print("Hello world!")
 
 
