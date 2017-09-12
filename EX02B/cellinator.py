aa165b63897a2ff84209c503ceac7008c57c60f3
Martin <masirg@ttu.ee>
2017-09-12 14:35:40 +0300

    v1

---
+++ EX02B/cellinator.py
@@ -0,0 +1,56 @@
+"""Conversion from row, col => cell index and vice versa."""
+
+
+def get_cell_index(row, col, row_len):
+    """
+    Return cell index for row, col and table width.
+
+    Given the row and column indices and the number of columns in the table
+    return cell index at the given location.
+
+    :param row: row index
+    :param col: column index
+    :param row_len: number of columns in the table
+    :return: cell index at the given location
+    """
+
+    return col + row_len * row
+
+
+def get_row_and_col(cell_index, row_len):
+    """
+    Return row, col for cell index and table width.
+
+    Given the cell index and the number of columns in the table
+    return tuple with row and col indices.
+
+    :param cell_index: cell index
+    :param row_len: number of columns in the table
+    :return: row index, col index as tuple
+    """
+    tup = (cell_index // row_len, cell_index % row_len)
+
+    return tup
+
+
+def get_row_len(row, col, cell_index):
+    """
+    Return table width for row, col and cell index.
+
+    Given the row and column indices and the cell index
+    return the number of columns in the table.
+
+    If the given setup is not possible
+    or it is not possible to deduct column count
+    return -1
+
+    :param row: row index
+    :param col: column index
+    :param cell_index: cell index
+    :return: number of columns in the table
+    """
+    row_len = (cell_index - col) / row
+    if col > row_len:
+        return - 1
+    else:
+        return row_len
