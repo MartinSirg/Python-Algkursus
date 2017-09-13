5b47b4e9563ebfd4eda43b8661617591b4145e2f
masirg <masirg@ttu.ee>
2017-09-13 11:28:13 +0300

    Pls work

--- EX04A/sorting.py
+++ EX04A/sorting.py
@@ -1,20 +1,6 @@
 """Sort the strings in ascending order."""
 
 
-def sort_list(string_list):
-    """
-    Function to sort string_list by the length of it's elements.
-
-    This function must utilize get_min_len_word().
-
-    :param string_list: List of Strings to be sorted.
-    :return: Sorted list of Strings.
-    """
-    new_list = string_list
-    new_list.sort(key=len)
-    return new_list
-
-
 def get_min_len_word(string_list):
     """
     Function to find and return the minimum length word in string_list.
@@ -36,4 +22,18 @@ def get_min_len_word(string_list):
             present_list.append(new_list[i])
         else:
             break
-    return present_list[0]
+    return present_list
+
+
+def sort_list(string_list):
+    """
+    Function to sort string_list by the length of it's elements.
+
+    This function must utilize get_min_len_word().
+
+    :param string_list: List of Strings to be sorted.
+    :return: Sorted list of Strings.
+    """
+    new_list = string_list
+    new_list.sort(key=len)
+    return new_list
