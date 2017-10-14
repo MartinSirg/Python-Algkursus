122d7fd05d0bfacd1fa108c127edd66bd217902e
masirg <masirg@ttu.ee>
2017-10-14 22:56:52 +0300

    v1

---
+++ EX06B/newton_tests.py
@@ -0,0 +1,31 @@
+import newton
+
+def test_newton_normal():
+    """Test function under normal conditions."""
+    assert newton.square_root_with_newton_method(4, 1) == 2
+
+
+def test_newton_number_zero():
+    """Test function if number is 0."""
+    assert newton.square_root_with_newton_method(0, 1) is None
+
+
+def test_newton_iterations_zero():
+    """Test function if iterations is 0."""
+    assert newton.square_root_with_newton_method(16, 0) == 8
+
+
+def test_newton_number_negative():
+    """Test function if number is negative."""
+    assert newton.square_root_with_newton_method(-1, 20) is None
+
+
+def test_newton_iterations_negative():
+    """Test function if iterations is negative."""
+    assert newton.square_root_with_newton_method(20, -1) is None
+
+
+def test_newton_decimal_points():
+    """Check if the returned value has a maximum of 3 decimal points."""
+    assert newton.square_root_with_newton_method(401, 10) == 20.025
+
