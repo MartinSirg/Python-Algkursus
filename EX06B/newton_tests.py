4cd6e5e58fc82d8e281dae967049f713f851d622
Martin <masirg@ttu.ee>
2017-10-15 22:42:27 +0300

    v2

--- EX06B/newton_tests.py
+++ EX06B/newton_tests.py
@@ -38,27 +38,30 @@ def test_newton__points():
 
 
 def test_newton_high_iterations():
-    """Test function with a high iteration"""
+    """Test function with a high iteration."""
     assert newton.square_root_with_newton_method(20, 1000) == 4.472
 
 
 def test_newton_high_numbers():
-    """Test function with a high iteration"""
+    """Test function with a high number."""
     assert newton.square_root_with_newton_method(21032, 10) == 145.024
 
 
 def test_newton_small_numbers():
-    """Test function with a small number"""
+    """Test function with a small number."""
     assert newton.square_root_with_newton_method(0.0000001, 10) == 0.002
 
 
 def test_newton_same_1():
+    """Test function when both number and iterations are the same negative number."""
     assert newton.square_root_with_newton_method(-10, -10) is None
 
 
 def test_newton_same_2():
+    """Test function when number and iterations are both 0."""
     assert newton.square_root_with_newton_method(0, 0) is None
 
 
 def test_newton_float():
+    """Test function when number is a float"""
     assert newton.square_root_with_newton_method(100.31, 10) == 10.015
