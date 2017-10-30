3721bc93ab20b520e3796ce8a205b6de3b379552
Martin <masirg@ttu.ee>
2017-10-30 14:33:23 +0200

    v0.5

--- EX08B/encoder.py
+++ EX08B/encoder.py
@@ -7,7 +7,11 @@ symbols = "/#$%^&*@0123456789"
 def _correct_message(message: str) -> str:
     """
     Correct message.
-    Take out all the symbols between spaces, unless they are at the end or in the beginning
+    Take out all the symbols between spaces, unless they are at the end or in the beginning.
+    Divide the message into three sections:
+    1.st section - symbols in the beginning of the word
+    2.nd section - text between beginning and end sections
+    3.rd section - symbols in the end
     :param message:
     :return:
     """
@@ -16,32 +20,33 @@ def _correct_message(message: str) -> str:
     for word in msg_list:
         first_section = ""
         last_section = ""
-        for i in range(len(word)):      # First section
+        for i in range(len(word)):                      # Make first section
             if word[i] in symbols:
                 first_section += word[i]
             else:
                 break
-        for i in range(len(word)):
+        for i in range(len(word)):                      # Make last section
             if word[-i - 1] in symbols:
                 last_section = word[-i - 1] + last_section
             else:
                 break
-        if len(first_section) > 0:
-            index1 = len(first_section)
+        if len(first_section) > 0:                      # Make middle section with word[index1: index2]
+            index1 = len(first_section)                 # Figure out index 1
         else:
             index1 = 0
-        index2 = len(word) - len(last_section)
-        middle_section = word[index1:index2]
-        new_mid = ""
-        for i in range(len(middle_section)):
-            if middle_section[i] not in symbols:
+        index2 = len(word) - len(last_section)          # Figure out index 2
+        middle_section = word[index1:index2]            # Define middle section
+        new_mid = ""                                    # Soon to be corrected middle section
+        for i in range(len(middle_section)):            # Loop through middle
+            if middle_section[i] not in symbols:        # Take out all given symbols
                 new_mid += middle_section[i]
-        if first_section == last_section and len(new_mid) == 0:
+        if first_section == last_section and len(new_mid) == 0:     # Exception for when a word consists of only symbols
             result_word = first_section
         else:
-            result_word = first_section + new_mid + last_section
-        new_list.append(result_word)
-    return " ".join(new_list)
+            result_word = first_section + new_mid + last_section    # Final result of a new word under normal cond.
+        new_list.append(result_word)                                # Add new word to list
+    return " ".join(new_list)                                       # Join the list together
+
 
 def _encrypt_message(message, shift):
     """
@@ -56,17 +61,28 @@ def _encrypt_message(message, shift):
     """
     if shift >= 26:
         shift %= 26
-    lower = string.ascii_lowercase
-    upper = string.ascii_uppercase
-    shifted_lower = lower[shift:] + lower[:shift]
+    lower = string.ascii_lowercase                  # Make lower case alphabet string
+    upper = string.ascii_uppercase                  # Make upper case alphabet string
+    shifted_lower = lower[shift:] + lower[:shift]   # Shift the alphabets
     shifted_upper = upper[shift:] + upper[:shift]
-    message = message.split()
-    """for word in message:
+    message = message.split()                       # Message string -> list
+    new_message = []                                # Soon ot be filled new message list
+    for word in message:
+        new_word = ""                               # using index() method: string.index("char")
         for i in range(len(word)):
-            if word[i] in lower:
+            if word[i] in lower:                    # When iterable char is lower case
+                index = lower.index(word[i])
+                new_word += shifted_lower[index]
+            elif word[i] in upper:                  # When iterable char is upper case
+                index = upper.index(word[i])
+                new_word += shifted_upper[index]
+            else:                                   # Symbols, numbers and other
+                new_word += word[i]
+        new_message.append(new_word)
+    return " ".join(new_message)
 
-            if word[i] in upper:
 
-            if word[i] not in string.ascii_letters:
-
-_encrypt_message("KEKEKE KEKEK KEK KEKEK KEKE", 1)"""
+def get_corrected_encrypted_message(initial_message, shift):
+    fixed_message = _correct_message(initial_message)
+    encrypted_message = _encrypt_message(fixed_message, shift)
+    return encrypted_message
