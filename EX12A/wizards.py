07e6f933092ddcad203d880bb856a5a7f0fc24fc
Martin <masirg@ttu.ee>
2017-11-23 15:19:09 +0200

    v1.22

--- EX12A/wizards.py
+++ EX12A/wizards.py
@@ -56,13 +56,9 @@ class Wizard:
         self.name = name
         if wand is None:
             self.wand = None
-        if wand is not None:
-            attribute1, attribute2 = wand.split(", ")
-            wand = Wand(attribute1,attribute2)
-            if wand.check_wand(wand) is not None:
-                raise MismatchError("The wand like that does not exist!")
-            else:
-                self.wand = wand
+        else:
+            wand.check_wand(wand)
+            self.wand = wand
 
     def set_wand(self, wand):
         """Give wizard a new wand if wand is correctly formatted."""
