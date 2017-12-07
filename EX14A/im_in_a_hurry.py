0cbc49a3d94e80c8111128d6b982f001daff210d
Martin <masirg@ttu.ee>
2017-12-07 22:59:53 +0200

    v1

---
+++ EX14A/im_in_a_hurry.py
@@ -0,0 +1,78 @@
+"""Retrieve stops and departures info from REST service."""
+
+import json
+import urllib.request
+
+API_BASE = "https://public-transport-api.herokuapp.com"
+REGION = "tallinn"
+raja_keemia = "tal_03504-1"
+
+
+def get_nearby_stops(api_base, lat, lng):
+    """
+    Get nearby stops.
+
+    :param api_base: Base URL that the endpoint gets appended to
+    :param lat: Latitude
+    :param lng: Longitude
+    :return: List of all nearby stops
+    """
+    with urllib.request.urlopen(f"{api_base}/stops/{lat}/{lng}") as file:
+        contents = file.read()
+        data = json.loads(contents.decode("utf-8"))
+        distance = []   # find shortest dist.
+        for item in data:
+            distance.append(int(item['distance'][:-2]))
+        result = []
+        while len(distance) > 1:
+            index_of_min_len = distance.index(min(distance))
+            result.append(data[index_of_min_len])
+            distance.remove(distance[index_of_min_len])
+            data.remove(data[index_of_min_len])
+        return result
+
+
+def get_nearest_stop(api_base, lat, lng):
+    """
+    Get nearest stop.
+
+    :param api_base: Base URL that the endpoint gets appended to
+    :param lat: Latitude
+    :param lng: Longitude
+    :return: Nearest stop
+    """
+    result = get_nearby_stops(api_base, lat, lng)
+    if len(result) == 0:
+        return None
+    return result[0]
+
+
+def get_next_departures(api_base, region, stop_id):
+    """
+    Get next departures from stop.
+
+    :param api_base: Base URL that the endpoint gets appended to
+    :param region: Region
+    :param stop_id: Stop ID
+    :return: List of next departures from stop
+    """
+    with urllib.request.urlopen(f"{api_base}/departures/{region}/{stop_id}") as file:
+        contents = file.read()
+        decoded_contents = contents.decode("utf-8")
+        data = json.loads(decoded_contents)
+        return data["departures"]
+
+
+def get_next_departure(api_base, region, stop_id):
+    """
+    Get next departure from stop.
+
+    :param api_base: Base URL that the endpoint gets appended to
+    :param region: Region
+    :param stop_id: Stop ID
+    :return: Next departure from stop
+    """
+    result = get_next_departures(api_base, region, stop_id)
+    if len(result) == 0:
+        return None
+    return result[0]
