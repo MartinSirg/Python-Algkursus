f9ee7268e84441a02c840ebf3e8ee209513f1e27
masirg <masirg@ttu.ee>
2017-12-14 16:41:05 +0200

    v1.1

---
+++ EX14B/it.py
@@ -0,0 +1,63 @@
+"""I LOVE MATH."""
+import math
+
+
+def get_lines(initial_line_length: float):
+    """"Quadratic equation, solve for y.
+
+    x is given as initial_line_length
+    y has to be shorter than x-y
+    :return y and x-y as tuple
+    """
+    x = initial_line_length     # y^2 - 3xy + x^2
+    b = 3 * x
+    c = x ** 2
+    y1 = round((b + math.sqrt(b**2 - 4 * c)) / 2)
+    y2 = round((b - math.sqrt(b**2 - 4 * c)) / 2)
+    if y1 <= int(x - y1):
+        return y1, x - y1
+    else:
+        return y2, x - y2
+
+
+def finder(row, col):
+    """Find table cell value.
+
+    example table:
+       col 1    2    3    4     5
+    row +----+----+----+----+-----+
+     1  | 1  | 2  | 4  | 7  | 11  |
+        +----+----+----+----+-----+
+     2  | 3  | 5  | 8  | 12 | 17  |
+        +----+----+----+----+-----+
+     3  | 6  | 9  | 13 | 18 | 24  |
+        +----+----+----+----+-----+
+     4  | 10 | 14 | 19 | 25 | 32  |
+     :return cell value
+     """
+    diagonal_nr = col + row - 1
+    count = 0
+    for i in range(1, diagonal_nr):
+        count += i
+    count += row
+    return count
+
+
+def clocky(hour, minute):
+    """
+    Calculate the angle between the hour and minute pointer.
+
+    :param hour: has to be between 1 and 12 inclusive
+    :param minute: has to be between 0 and 59 inclusive
+    :return: angle
+    """
+    if hour > 12 or hour < 1 or minute > 59 or minute < 0:
+        return -1
+    if hour == 12:
+        hour = 0
+    else:
+        hour = hour * 30
+    minute = minute * 6
+    if hour - minute < 0:
+        return minute - hour
+    return hour - minute
